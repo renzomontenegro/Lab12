0e09d1cacc977d3c21fc1718bdee7218aa96a8b4
renzomontenegro <renzomogra@gmail.com>
2024-06-01 12:14:31 -0500

    Register & Login con Firebase 2

--- app/src/main/java/dev/renzomont/lab12/RegisterActivity.kt
+++ app/src/main/java/dev/renzomont/lab12/RegisterActivity.kt
@@ -65,7 +65,7 @@ class RegisterActivity : AppCompatActivity() {
                     }
                 }
         }
-
+        //comentario
         btnLogeate.setOnClickListener{
             startActivity(
                 Intent(this,
